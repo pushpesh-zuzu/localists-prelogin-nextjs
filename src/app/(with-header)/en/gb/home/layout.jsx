d5849d0a168dc28f2166312735a9e9a2644d8536
Bade Harish <badeharish@zuzucodes.com>
2026-01-22 21:20:05 +0530

    update home page layout

--- src/app/(with-header)/en/gb/home/layout.jsx
+++ src/app/(with-header)/en/gb/home/layout.jsx
@@ -4,13 +4,6 @@ export const metadata = {
     title: "Find Trusted Home & Garden Professionals Near Me - Localists",
     description:
         "Need help finding Home & Garden professionals, consultants, or expert local services near you? Get free quotes now at Localists. It's quick, easy & free.",
-
-    alternates: {
-        // canonical: "https://dev2.localistsbooster.com/en/gb/home",
-        languages: {
-            "en-GB": "https://dev2.localistsbooster.com/en/gb/home"
-        },
-    },
 };
 
 
