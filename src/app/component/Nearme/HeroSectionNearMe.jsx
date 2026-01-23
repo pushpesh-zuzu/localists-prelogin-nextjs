07ffd1941fafb457972ab7a72d8138bc17b3f0f4
Deepak Birla <deepakbirla@zuzucodes.com>
2026-01-23 14:28:40 +0530

    update-tree-surgeon-banner

--- src/app/component/Nearme/HeroSectionNearMe.jsx
+++ src/app/component/Nearme/HeroSectionNearMe.jsx
@@ -26,11 +26,11 @@ function HeroSectionNearMe({
   return (
     <>
       <WrapperBGWidth
-        secondaryClass="md:h-[350px] lg:h-[650px] xl:h-[690px] max-h-[690px]"
-        className="relative md:h-[350px] lg:h-[650px] xl:h-[690px] max-h-[690px]"
+        secondaryClass="md:h-[340px] lg:h-[513px] xl:h-[570px] max-h-[570px]"
+        className="relative md:h-[340px] lg:h-[513px] xl:h-[570px] max-h-[570px]"
         background={"#00afe3"}
       >
-        <div className="absolute inset-0 md:h-[350px] lg:h-[650px] xl:h-[690px] max-h-[690px]  hidden md:block">
+        <div className="absolute inset-0 md:h-[340px] lg:h-[513px] xl:h-[570px] max-h-[570px]  hidden md:block">
           <Image
             src={bannerImage}
             alt={altText}
@@ -38,7 +38,7 @@ function HeroSectionNearMe({
             priority={true}
             fetchPriority="high"
             quality={75}
-            className="h-10 w-10 md:h-[350px] min-[900px]:h-[400px] lg:h-[650px] xl:h-[690px] md:max-w-[1536px] mx-auto"
+            className="h-10 w-10 md:h-[340px] min-[900px]:h-[400px] lg:h-[513px] xl:h-[570px] md:max-w-[1536px] mx-auto"
             sizes="(min-width: 768px) 100vw, 0px"
           />
         </div>
@@ -56,22 +56,22 @@ function HeroSectionNearMe({
           />
         </div>
         <section
-          className="flex flex-col max-h-[550px] max-[353px]:max-h-[550px] md:max-h-full  md:flex-row px-[34px] pt-10 md:px-[60px] lg:pb-[64px] xl:pb-[100px] md:pt-4 xl:px-[120px] xl:pt-[73.88px] relative"
+          className="flex flex-col max-h-[550px] max-[353px]:max-h-[550px] md:max-h-full  md:flex-row px-[34px] pt-10 md:px-[60px] lg:pb-[64px] xl:pb-[100px] md:pt-5 lg:pt-8 xl:px-[120px] xl:pt-[73.88px] relative"
           role="banner"
           aria-label="Hero section"
         >
           <div
-            className="w-full md:w-[85%] lg:w-[84%]  xl:w-[65%]
+            className="w-full
            flex flex-col  relative z-10"
           >
-            <TrustpioletIcon className="max-w-[184px] md:max-w-[177px] lg:max-w-[330px] md:mb-3 lg:mb-[30px] max-h-12" />
+            <TrustpioletIcon className="lg:min-h-[54px] max-h-[28px] max-w-[184px] md:max-w-[177px] lg:max-w-[360px] md:mb-3 lg:mb-[30px] lg:max-h-12" />
             <H1  className="text-white block drop-shadow-[0px_4px_4px_rgba(0,0,0,0.1)] ">
               <span >
                 {heading1}
                 <span className="text-[#253238] block">{heading2}</span>
               </span>
             </H1>
-            <Paragraph1 className="text-shadow-[0px 0px 4px 0px #00000040] md:text-shadow-[0px 4px 4px 0px #00000026] md:max-w-[80%] sm:max-w-[60%] max-w-full text-white pt-[18px] md:pt-4 lg:pt-[20px] text-shadow-[0_0_4px_rgba(0,0,0,0.25)]">
+            <Paragraph1 className="text-shadow-[0px 0px 4px 0px #00000040] md:text-shadow-[0px 4px 4px 0px #00000026] md:max-w-[80%] sm:max-w-[60%] md:max-w-[450px] lg:max-w-[590px] text-white pt-[18px] md:pt-4 lg:pt-[20px] text-shadow-[0_0_4px_rgba(0,0,0,0.25)]">
               {description}
             </Paragraph1>
             <PostCodeSearchField />
