bab766e79fc574f97c1ab5dce6576e751eb912a4
Deepak Birla <deepakbirla@zuzucodes.com>
2026-01-23 11:04:10 +0530

    update-tree-surgeon-near-me-tablete-bannner

--- src/app/component/Nearme/HowItWorkNearMe.jsx
+++ src/app/component/Nearme/HowItWorkNearMe.jsx
@@ -12,7 +12,7 @@ function HowItWorkNearMe() {
   return (
     <WrapperBGWidth background={"white"}>
       <section className="bg-white h-auto px-2.5 pt-12 pb-[30px] sm:px-10 md:px-16 md:pb-10 md:pt-0 xl:px-[145px] xl:pb-[72px]">
-        <H2 className="text-[#00afe3] pb-[30px] md:pb-6 xl:pb-[47.89px]">
+        <H2 className="text-[#00afe3] pb-[30px] md:pb-[35px] xl:pb-[47.89px]">
           How it <span className="text-[#253238]">Works.</span>{" "}
         </H2>
         <div className="flex flex-col md:flex-row justify-items-start gap-5 xl:gap-[50px]">
