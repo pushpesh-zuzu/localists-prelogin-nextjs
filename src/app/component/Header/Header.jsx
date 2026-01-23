5b28d35e38af8522fa7966a64f2b8b987c395aff
Deepak Birla <deepakbirla@zuzucodes.com>
2026-01-24 01:27:08 +0530

    remove-explore-text

--- src/app/component/Header/Header.jsx
+++ src/app/component/Header/Header.jsx
@@ -51,7 +51,7 @@ export default function Header() {
             <div className="flex items-center space-x-4 md:space-x-2 lg:space-x-2.5 lg:py-3">
               <MegaMenu>
                 <button className="flex items-center sm:gap-1 lg:gap-1.5  text-[12px] lg:text-base font-bold whitespace-nowrap text-[#253238]">
-                  Explore Our Services
+                  {/* Explore Our Services */}
                   {/* <Image
                     src="/icons/downarrowblue.svg"
                     alt="down-arrow"
