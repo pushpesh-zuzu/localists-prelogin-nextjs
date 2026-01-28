a68ac57d83deff74a65dbc1b698c15ad00a57f4d
Bade Harish <badeharish@zuzucodes.com>
2026-01-28 21:27:35 +0530

    add driveway half page

---
+++ src/app/component/Nearme/DrivewayInstallers/DrivewayInstallersData.js
@@ -0,0 +1,334 @@
+export const QUESTION_AND_ABOUT_SERVICE = [
+    {
+        type: "image",
+        src: "/nearme/treesurgery1.webp",
+        alt: "a tree surgeon performing some crown reduction on a tree",
+        marginBottom: true,
+    },
+    {
+        type: "h2",
+        text: "How much does a tree surgeon cost",
+    },
+    {
+        type: "p",
+        text: "The cost of hiring a tree surgeon depends on several factors, including the size and type of tree, the complexity of the job, and your location. Small pruning jobs for garden trees are generally less expensive. However, removing large or hazardous trees requires more time, equipment, and expertise, which increases the cost. Also, in terms of location, specialists in larger cities like Manchester or high-demand areas may charge more than those in rural regions.",
+    },
+    {
+        type: "p",
+        text: "Other considerations that can affect pricing include accessibility (how easy it is to reach the tree), whether stump removal is needed, and if the job requires special safety equipment or multiple hands on site.",
+    },
+    {
+        type: "p",
+        text: "At Localists, you don’t need to guess or spend hours searching for reliable tree surgeons. Simply submit your requirements and receive free quotes from qualified local experts to your inbox. You can then compare prices, experience, and availability, and see what fits your needs.",
+    },
+    {
+        type: "AveratePriceSection",
+    },
+    {
+        type: "image",
+        src: "/nearme/treesurgery.webp",
+        alt: "A tree surgeon climbing to the top of a tree with chainsaw in hand",
+        marginBottom: true,
+        // marginTop:true
+    },
+    {
+        type: "h2",
+        text: "What does a tree surgeon do?",
+    },
+    {
+        type: "p",
+        text: "A tree surgeon, sometimes called an arborist, is a skilled professional whose work keeps trees healthy, safe, and integral to your landscape. Whether it’s shaping, pruning, or you’re looking for a complete tree removal, their skills ensure your outdoor space is well cared for.",
+    },
+    {
+        type: "p",
+        text: "Trees can become harmful when they’re diseased, unstable, or simply too large for their surroundings. For example, a tree with hidden rot at the base might look fine on the outside but could topple in a storm, damaging property or blocking a road. In other cases, overgrown branches might stretch into power lines, or roots can push up paving slabs and create trip hazards. This is where a tree surgeon’s expertise matters. They can spot these risks early and take action.",
+    },
+    {
+        type: "pbold",
+        text: "Here are some of the key things a tree surgeon does:",
+        className: "my-[15px] md:my-[25px]",
+    },
+    { type: "pbold", text: "1. Tree health and safety checks" },
+    {
+        type: "p",
+        text: "Before any work begins, a tree surgeon assesses the overall health and stability of your trees. They’re trained to spot early signs of disease, pest infestations, cracks, or hidden rot that most of us would overlook. ",
+    },
+    { type: "pbold", text: "2. Pruning, crown work & structural trimming" },
+    {
+        type: "p",
+        text: "Pruning isn’t just about making a tree look neat. It’s about keeping it healthy and safe. Tree surgeons handle this carefully, making precise cuts to support long-term growth and stability. Whether it’s reducing the crown to let in more light, lifting branches to clear space around buildings, or removing overgrowth, tree surgeons know how to balance a tree’s structure.",
+    },
+    { type: "PostCodeSection" },
+    {
+        type: "pbold",
+        text: "3. Safe tree removal",
+    },
+    {
+        type: "p",
+        text: "Sometimes a tree is simply too diseased, damaged, or unstable to save. In these cases, tree surgeons carry out removals safely using specialist climbing gear, cranes, ropes, and dismantling techniques. They know how to handle this professionally, keeping health and safety in mind, to prevent damage to people, property, or surrounding plants.",
+        className: "pb-[5px] md:pb-0"
+    },
+
+    {
+        type: "pbold",
+        text: "4. Stump grinding & root work",
+    },
+    {
+        type: "p",
+        text: "Taking down a tree is only half the job. The stump left behind can become a tripping hazard, attract pests, or sprout regrowth. Tree surgeons use professional stump grinders to remove it completely, making space for new planting, landscaping, or simply a tidy, safer garden.",
+        className: "pb-[5px] md:pb-0"
+
+    },
+    {
+        type: "pbold",
+        text: "5.  Planting & conservation",
+    },
+    {
+        type: "p",
+        text: "Tree surgeons don’t just remove trees. They help plant them too. They can recommend the right species for your soil, plant them correctly and even transplant existing trees when needed.",
+        className: "pb-[5px] md:pb-0"
+
+    },
+
+    {
+        type: "pbold",
+        text: "6. Disease & pest management",
+    },
+    {
+        type: "p",
+        text: "Trees can suffer from pests, fungi, and decay. Tree surgeons identify these problems early and recommend treatments or interventions to prevent further spread.",
+        className: "pb-[5px] md:pb-0"
+
+    },
+    {
+        type: "pbold",
+        text: "7. Emergency response",
+    },
+    {
+        type: "p",
+        text: "Storm damage or fallen branches can pose immediate risks. Tree surgeons are often called out in emergencies to make trees safe, clear debris, and restore access quickly.",
+        className: "pb-[5px] md:pb-0"
+
+    },
+
+    {
+        type: "pbold",
+        text: "8. Surveys & reports",
+    },
+    {
+        type: "p",
+        text: "For planning applications, insurance, or legal compliance, a tree surgeon (or arborist) can provide formal surveys and written reports to assess safety, preservation needs, or risks.",
+        notMarginBottom: true,
+    },
+    {
+        type: "image",
+        src: "/nearme/sergeondo.webp",
+        alt: "a tree surgeon hanging from a tree whilst pressing buttons on his chainsaw",
+        marginTop: true,
+        marginBottom: true,
+    },
+    {
+        type: "h2",
+        text: "How do I pick the right tree surgeon",
+    },
+    {
+        type: "pbold",
+        text: "When choosing a tree surgeon, it’s important to look beyond price and focus on their training, safety standards, and experience. You can look out for one or more of the following:",
+        className: "mb-[15px] md:mb-[25px] ",
+    },
+    {
+        type: "li",
+        text: "Insurance - This is also non-negotiable as it protects you and your property if something goes wrong.",
+        className: "mr-5",
+    },
+    {
+        type: "li",
+        text: "Reviews - Check reviews and recommendations from past clients to see how reliable they are in practice.",
+        className: "mr-5",
+    },
+    {
+        type: "li",
+        text: "Transparency - A good tree surgeon will be transparent: they’ll explain the work clearly, provide a written quote, and answer your questions before starting.",
+        className: "mr-5",
+    },
+    {
+        type: "li",
+        text: "Relevant experience - Having hands-on experience with the type of job you need is also a plus. Whether that’s caring for ornamental trees in your garden, managing large oaks on a commercial site, or maintaining multiple trees in public spaces.",
+        className: "mr-5",
+    },
+    {
+        type: "li",
+        text: `Professional bodies membership –  Having a membership to organisations such as the <a href='https://www.trees.org.uk/' target='_blank' rel='noopener noreferrer' style='color:#00AFE3;'>Arboricultural Association</a> often means they have to adhere to strict regulations and protocols.`,
+        className: "mr-5",
+    },
+];
+export const FREQUENTLY_DATA_TREE_SURGEON = {
+    "tree-surgeons-near-me": [
+        {
+            key: "1",
+            title: "What is the best time of year for tree surgery?",
+            description: `
+      Usually, the best time for most tree surgery would be late autumn to early spring, when trees are dormant. With fewer leaves and less sap flow, the work is not only easier on the tree and the tree surgeon, but often cheaper too. Demand is usually lower outside of peak gardening season, so you may be able to save on costs.
+      <br /><br />
+      Of course, if a tree is diseased, unsafe, or posing a risk, it’s important to act straight away. Don’t wait for the “cheapest” time. Safety should always come first, and a qualified tree surgeon can guide you on the best approach for your situation.
+      `,
+        },
+        {
+            key: "2",
+            title: "When should you worry about a tree?",
+            description: `
+      You should start to worry about a tree if you notice signs that it might be unhealthy or unstable. Things like large cracks in the trunk, branches that look dead or brittle, fungus or mushrooms growing around the base, or the tree leaning more than usual are all red flags. Roots lifting pavings, or damaging walls can also be a concern.
+      <br /> <br />
+      Sometimes the warning signs aren’t that obvious though. For example, rot can be hidden inside the trunk. If you’re unsure, it’s always safer to have a professional tree removal expert take a look. They can quickly tell you whether the tree just needs some pruning, or if it’s a bigger issue that requires urgent action.
+      `,
+        },
+        {
+            key: "3",
+            title: "What trees can be cut down without permission in the UK?",
+            description: `
+      Not all trees can be removed freely. If a tree is protected by a Tree Preservation Order (TPO) or is in a conservation area, you’ll need council permission before carrying out work. Cutting down or heavily pruning without approval could lead to hefty fines. If you’re unsure, a tree surgeon can check the status for you and guide you through the process.`,
+        },
+        {
+            key: "4",
+            title: "How much does it cost to reduce the height of a tree in the UK?",
+            description: `
+      Reducing a tree’s height (also called crown reduction) can cost anywhere between <strong>£150 and £500</strong>, depending on the size of the tree, how much needs to be reduced, and where you live. For example, reducing a very tall oak will cost more than trimming back a medium-sized birch. Tree surgeons usually visit first to assess and give a tailored quote.`,
+        },
+
+    ],
+};
+export const TREE_SRUGEON_ADVICEINSIGHT = [
+    {
+        id: 1,
+        title: "How to remove a tree stump",
+        image: "/nearme/tree-remove.webp",
+    },
+    {
+        id: 2,
+        title: " How to tell if a tree is dying",
+        image: "/nearme/trees-removal.webp",
+    },
+    {
+        id: 3,
+        title: "How much does it cost to remove a tree?",
+        image: "/nearme/tree-climb-remov.webp",
+    },
+    {
+        id: 4,
+        title: "What is a tree surgeon?",
+        image: "/nearme/long-tree-remove.webp",
+    },
+    {
+        id: 5,
+        title: " What is an arborist?",
+        image: "/nearme/tree-root-remove.webp",
+    },
+];
+export const DRIVEWAY_INSTALLERS_TABDATA = {
+    professionals: [
+        "Driveway Installers North Yorkshire",
+        "Driveway Installers Liverpool",
+        "Driveway Installers Manchester",
+        "Driveway Installers Chester",
+        "Driveway Installers Leeds",
+        "Driveway Installers York",
+        "Driveway Installers Harrogate",
+        "Driveway Installers Cheshire",
+        "Driveway Installers Hull",
+        "Driveway Installers Stockport",
+        "Driveway Installers Bolton",
+    ],
+    popular: [
+        "Landscapers Near Me",
+        "Gardeners Near Me",
+        "Fence and Gate Installation Near Me",
+        "Architects Near Me",
+        "Gutter Cleaners Near Me",
+        "Artificial Grass Installers Near Me",
+    ],
+    insight: [
+
+    ],
+};
+export const VETTED_DATA_DRIVEWAY_INSTALLER = [
+    `Bumps in life? inevitable. Bumps in your driveway? fixable. If you're looking to smooth things over with your driveway and give your car the VIP treatment, then ditch the red carpet for a brand new tarmac, resin or block-paved driveway.`,
+    `At Localists, we connect you with local driveway contractors who can pamper your neglected driveway back into shape. Just give us a few details, and we'll get you up to 5 free quotes straight into your inbox. No cost. No obligation. No fuss. Just sit back and let us do the legwork for you.`,
+];
+
+export const BREADCRUM_DATA_DRIVEWAY = [
+    { title: "Home & Garden", path: "/en/gb/home" },
+    { title: "Driveway Installers" },
+];
+
+export const POPULAR_SERVICES = [
+    "Driveway resurfacing",
+    "Driveway repairs",
+    "Driveway drainage installation",
+    "Driveway cleaning",
+    "Driveway sealing & resealing",
+    "Driveway edging installation",
+    "Driveway weed & moss removal",
+];
+export const DRIVEWAY_INSTALLERS_FEEDBACK = [
+    {
+        id: 1,
+        text: "Within minutes of submitting my request, I heard back from two local companies ready to help. Couldn’t have asked for a quicker service.",
+        name: "Amelia",
+    },
+    {
+        id: 2,
+        text: "Had 3 quotes from local driveway installers, and got the work done the same day for less than originally quoted! Will definitely use again",
+        name: "Robert",
+    },
+    {
+        id: 3,
+        text: "Got instant quotes for my driveway -  and had the work done 2 weeks later. Can't fault them, will definitely use Localists again. ",
+        name: "Alan",
+    },
+    {
+        id: 4,
+        text: "Couldn't fault the service, saved me shopping around for a cheaper quote. Will definitely use again!",
+        name: "Tracey",
+    },
+];
+
+export const TREE_SURGEON_DATA = [
+    {
+        id: 1,
+        title: "Home & Garden",
+        image: "/homepage/image6.webp",
+        isSpecial: true,
+        bgColor: "bg-cyan-400"
+    },
+    {
+        id: 2,
+        title: "Fence & Gate Installation",
+        image: "/homepage/fenceandgate.webp",
+        isSpecial: false
+    },
+    {
+        id: 3,
+        title: "Landscaping",
+        image: "/homepage/landscapingservice.webp", // Aapki image path
+        isSpecial: false
+    },
+    {
+        id: 4,
+        title: "Artificial Grass Installation",
+        image: "/homepage/artificialgrassinstallation.webp",
+        isSpecial: false
+    },
+    {
+        id: 5,
+        title: "Patio Layer",
+        image: "/homepage/patiolayer.webp", // Aapki image path
+        isSpecial: false
+    },
+
+    {
+        id: 6,
+        title: "Gutter Cleaning",
+        image: "/homepage/guttercleaning.webp", // Aapki image path
+        isSpecial: false
+    },
+];
