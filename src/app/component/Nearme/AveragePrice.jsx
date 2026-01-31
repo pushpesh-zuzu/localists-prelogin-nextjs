8db75d1152e33b1b9f47946ba5dc42796fbd9317
Deepak Birla <deepakbirla@zuzucodes.com>
2026-01-31 22:23:56 +0530

    update-average-section

--- src/app/component/Nearme/AveragePrice.jsx
+++ src/app/component/Nearme/AveragePrice.jsx
@@ -12,8 +12,8 @@ export default function AveragePrice() {
         >
           <div className="flex flex-col lg:flex-row lg:gap-6">
             {/* Left Section - H2 and GetQuote */}
-            <div className="flex flex-col justify-between px-2.5  pt-[30px] lg:pl-[38px]  lg:pt-[54px] lg:pb-[52px] lg:min-w-[386px]">
-              <H2 className="text-center min-[460px]:max-w-[80%] min-[460px]:mx-auto md:mx-0 md:max-w-full lg:text-left">
+            <div className="flex flex-col justify-between px-2.5  pt-[30px] lg:pl-[38px]  lg:pt-[54px] lg:pb-[52px] lg:max-w-[35%] xl:max-w-full xl:min-w-[386px]">
+              <H2 className="text-center  min-[460px]:mx-auto md:mx-0 md:max-w-full lg:text-left">
                 Average cost of tree surgery UK
               </H2>
 
@@ -23,16 +23,16 @@ export default function AveragePrice() {
             </div>
 
             {/* Right Section - Price Grid */}
-            <div className="grid grid-cols-2 lg:grid-cols-2 gap-x-[14px] gap-y-5 lg:gap-x-5 px-[10px] min-[450px]:px-20 lg:px-0 pt-[23px] pb-[38px] lg:py-16 lg:pr-7">
+            <div className=" xl:max-w-[57%] grid grid-cols-2 lg:grid-cols-2 gap-x-[14px] gap-y-5 lg:gap-x-5 px-[10px] min-[450px]:px-20 lg:px-0 pt-[23px] pb-[38px] lg:py-16 lg:pr-7">
               {/* Price Card 1 */}
               <div className="">
                 <Paragraph
                   variant="VeryprimarySmall"
-                  className="md:mx-auto max-w-[110px]  min-[360px]:max-w-[120px] min-[360px]:min-w-[120px] min-[450px]:text-center min-[450px]:mx-auto md:text-center md:max-w-[160px] lg:max-w-full text-left lg:text-center"
+                  className="max-[760px]:min-h-15 max-[760px]:max-h-15  max-[1023px]:min-h-20 max-[1023px]:max-h-20  md:mx-auto  text-left min-[450px]:text-center min-[450px]:mx-auto md:text-center  md:max-w-[220px]  lg:max-w-[90%]  lg:text-center"
                 >
                   For a small tree removal (under 25ft), the average cost is
                 </Paragraph>
-                <p className=" text-4xl lg:text-5xl tracking-[-0.03em] font-black text-left min-[450px]:text-left md:text-center lg:text-center text-[#253238] mt-6 md:mt-3 lg:mt-3">
+                <p className="text-4xl lg:text-5xl tracking-[-0.03em] font-black text-center  md:text-center lg:text-center text-[#253238] lg:mt-3">
                   £450
                 </p>
               </div>
@@ -41,11 +41,11 @@ export default function AveragePrice() {
               <div className="">
                 <Paragraph
                   variant="VeryprimarySmall"
-                  className="tracking-[-0.03em]! md:mx-auto text-left min-[450px]:text-center min-[450px]:mx-auto  md:text-center lg:text-center max-w-[164px] md:max-w-[220px] lg:min-w-[264px]"
+                  className="max-[760px]:min-h-15 max-[760px]:max-h-15 max-[1023px]:min-h-20 max-[1023px]:max-h-20 tracking-[-0.03em]! md:mx-auto text-left min-[450px]:text-center min-[450px]:mx-auto  md:text-center lg:text-center  md:max-w-[220px] lg:min-w-[264px]"
                 >
                   For a medium stump removal under 15 inch diameter), the average cost is
                 </Paragraph>
-                <p className=" text-4xl lg:text-5xl tracking-[-0.03em] font-black text-left min-[450px]:text-left md:text-center text-[#253238] mt-3">
+                <p className=" text-4xl lg:text-5xl tracking-[-0.03em] font-black text-center md:text-center text-[#253238] lg:mt-3 ">
                   £215
                 </p>
               </div>
@@ -57,11 +57,11 @@ export default function AveragePrice() {
               <div className="h-full">
                 <Paragraph
                   variant="VeryprimarySmall"
-                  className="md:mx-auto text-left min-[450px]:text-center min-[450px]:mx-auto md:text-center lg:text-center max-w-[142px] md:max-w-[220px]  lg:max-w-[90%]"
+                  className="max-[760px]:min-h-15 max-[760px]:max-h-15 max-[1023px]:min-h-20 max-[1023px]:max-h-20 md:mx-auto text-left min-[450px]:text-center min-[450px]:mx-auto md:text-center lg:text-center   md:max-w-[220px]  lg:max-w-[90%]"
                 >
                   Trimming or pruning a small tree (under 25ft) costs on average
                 </Paragraph>
-                <p className=" text-4xl lg:text-5xl tracking-[-0.03em] font-black text-left min-[450px]:text-left md:text-center text-[#253238] max-[336px]:mt-[12px] mt-8 md:mt-3 sm:mt-4">
+                <p className=" text-4xl lg:text-5xl tracking-[-0.03em] font-black text-center md:text-center text-[#253238]  lg:mt-3">
                   £250
                 </p>
               </div>
@@ -70,11 +70,11 @@ export default function AveragePrice() {
               <div className="">
                 <Paragraph
                   variant="VeryprimarySmall"
-                  className="md:mx-auto text-left min-[450px]:text-center min-[450px]:mx-auto md:text-center lg:text-center max-w-[150px] md:max-w-[220px] lg:min-w-[264px]"
+                  className="max-[760px]:min-h-15 max-[760px]:max-h-15 max-[1023px]:min-h-20 max-[1023px]:max-h-20 md:mx-auto text-left min-[450px]:text-center min-[450px]:mx-auto md:text-center lg:text-center  md:max-w-[220px] lg:min-w-[264px]"
                 >
                   For medium stump grinding (under 15 inch in diameter), the average cost is
                 </Paragraph>
-                <p className=" text-4xl lg:text-5xl tracking-[-0.03em] font-black text-left min-[450px]:text-left md:text-center text-[#253238] mt-2.5 min-[331px]:mt-[18px] md:mt-3 mr-1 sm:mr-0">
+                <p className=" text-4xl lg:text-5xl tracking-[-0.03em] font-black text-center md:text-center text-[#253238] lg:mt-3">
                   £115
                 </p>
               </div>
