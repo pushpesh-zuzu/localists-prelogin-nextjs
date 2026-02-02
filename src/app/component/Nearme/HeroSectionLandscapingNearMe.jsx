836d17fc252e9b2577ad7acade7f245e2d662e34
Deepak Birla <deepakbirla@zuzucodes.com>
2026-02-02 17:26:29 +0530

    update-landscaping-gardener

---
+++ src/app/component/Nearme/HeroSectionLandscapingNearMe.jsx
@@ -0,0 +1,86 @@
+"use client";
+import React from "react";
+import WrapperBGWidth from "../common/WrapperBGWidth/WrapperBGWidth";
+import TrustpioletIcon from "../common/icons/HomePageIcons/TrustpioletIcon";
+import Paragraph1 from "../UI/Typography/Paragraph1";
+import H1 from "../UI/Typography/H1";
+import Image from "next/image";
+import ChevroliteDoubleDownIcon from "../common/icons/HomePageIcons/ChevroliteDoubleDownIcon";
+import PostCodeSearchField from "./PostCodeSearchField";
+
+function HeroSectionLandscapingNearMe({
+  heading1 = "Find Tree Surgeons",
+  heading2 = "Near You.",
+  description = `Find the best tree surgeons for your job, just enter your postcode
+              and a few details for instant quotes.`,
+  bannerImage = "",
+  bannerImageMobile = "",
+  altText = "Tree Surgeon",
+  serviceId = 112,
+  serviceName = "Tree Surgery",
+  containerHight = "relative w-full md:h-[375px] lg:h-[550px] xl:h-[570px] xl:max-h-[570px]",
+  desktopImageClass = "object-cover h-10 w-10 md:h-[375px] min-[900px]:h-[400px] lg:h-[550px] xl:h-[570px]",
+}) {
+  return (
+    <>
+      <div className={`${containerHight}`}>
+        <div className="absolute inset-0 hidden md:block z-0 w-full h-full">
+          <Image
+            src={bannerImage}
+            alt={altText}
+            fill
+            priority={true}
+            fetchPriority="high"
+            quality={90}
+            className={desktopImageClass}
+            sizes="(min-width: 768px) 100vw, 0px"
+          />
+        </div>
+
+        {/* Mobile Image */}
+        <div className="absolute inset-0 md:hidden h-full w-full z-0">
+          <Image
+            src={bannerImageMobile}
+            alt={altText}
+            priority
+            fetchPriority="high"
+            fill
+            quality={90}
+            sizes="100vw"
+            className="object-cover w-full h-full"
+          />
+        </div>
+
+        <div className="relative z-10 max-w-[1536px] mx-auto h-full">
+          <section
+            className="flex flex-col max-h-[550px] max-[353px]:max-h-[550px] md:max-h-full md:flex-row px-[30px] pt-10 md:px-[60px] lg:pb-[64px] xl:pb-[100px] md:pt-5 lg:pt-8 xl:px-[120px] xl:pt-[73.88px] relative z-10"
+            role="banner"
+            aria-label="Hero section"
+          >
+            <div className="w-full flex flex-col relative z-10">
+              <TrustpioletIcon className="lg:min-h-[54px] max-h-[28px] max-w-[184px] md:max-w-[177px] lg:max-w-[360px] mb-[16.54px] md:mb-3 lg:mb-[30px] lg:max-h-12" />
+              <H1 className="text-white block drop-shadow-[0px_4px_4px_rgba(0,0,0,0.1)]">
+                <span>
+                  {heading1}
+                  <span className="text-[#253238] block">{heading2}</span>
+                </span>
+              </H1>
+              <Paragraph1 className="tracking-[0em]! text-shadow-[0px 0px 4px 0px #00000040] md:text-shadow-[0px 4px 4px 0px #00000026] sm:max-w-[60%] md:max-w-[600px] lg:max-w-[700px] text-white pt-[18px] md:pt-3 lg:pt-[20px] text-shadow-[0_0_4px_rgba(0,0,0,0.25)]">
+                {description}
+              </Paragraph1>
+              <PostCodeSearchField
+                serviceId={serviceId}
+                serviceName={serviceName}
+              />
+            </div>
+            <div className="flex h-[12px] ml-auto mr-auto justify-center mb-3.5 mt-[30px] md:hidden relative z-1">
+              {/* <ChevroliteDoubleDownIcon /> */}
+            </div>
+          </section>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HeroSectionLandscapingNearMe;
