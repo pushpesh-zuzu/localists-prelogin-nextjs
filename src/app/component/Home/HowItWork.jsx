2e6a879506d5126066fe7f54dde59335654af4f7
Deepak Birla <deepakbirla@zuzucodes.com>
2026-02-03 12:16:48 +0530

    update-px

--- src/app/component/Home/HowItWork.jsx
+++ src/app/component/Home/HowItWork.jsx
@@ -44,7 +44,7 @@ function HowItWork() {
           />
         </div>
          <div className="flex justify-center">
-                  <Button variant="primary" onClick={() => { handleScrollToBottom() }} className="cursor-pointer rounded-full max-w-fit bg-[#10C87B] hover:bg-[#00aef3] text-white mt-[30px] lg:mt-[47px] px-[15px] py-2 xl:py-[15px] xl:px-7 leading-normal!">
+                  <Button variant="primary" onClick={() => { handleScrollToBottom() }} className="cursor-pointer rounded-full max-w-fit bg-[#10C87B] hover:bg-[#00aef3] text-white mt-[30px] lg:mt-[47px] px-[15px] md:px-[18px] py-2 xl:py-[15px] xl:px-8 leading-normal!">
                     Get Started
                   </Button>
                 </div>
