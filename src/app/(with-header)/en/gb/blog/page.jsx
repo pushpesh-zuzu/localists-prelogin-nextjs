91b3f1d75a63a4721cd2c0d1aa0d6e46f307f7bc
Bade Harish <badeharish@zuzucodes.com>
2026-02-05 18:13:26 +0530

    add postcode in blog page

--- src/app/(with-header)/en/gb/blog/page.jsx
+++ src/app/(with-header)/en/gb/blog/page.jsx
@@ -2,9 +2,12 @@ import BlogGridSection from "@/app/component/BlogArchive/BlogSection/BlogGridSec
 import HeroSection from "../../../../component/BlogArchive/HeroSection/HeroSection";
 import IndustryInsights from "@/app/component/BlogArchive/IndustryInsights";
 import CloseBrowserAbandon from "@/app/component/common/CloseBrowserAbandon/CloseBrowserAbandon";
-import DiscoverServices from "@/app/component/BlogArchive/DiscoverServices";
+// import DiscoverServices from "@/app/component/BlogArchive/DiscoverServices";
 import Footer from "@/app/component/Footer/Footer";
 import SEO from "@/app/component/common/seo/SEO";
+import DiscoverNearMe from "@/app/component/Nearme/DiscoverNearMe";
+import { BLOG_DATA } from "@/app/component/BlogArchive/BlogDiscoverData";
+import PostCodeSection from "@/app/component/Nearme/AboutServicesAndQuestions/PostCodeSection";
 
 
 export const metadata = {
@@ -31,7 +34,12 @@ export default function Page() {
             <HeroSection />
             <BlogGridSection />
             <IndustryInsights />
-            <DiscoverServices />
+            <DiscoverNearMe homeData={BLOG_DATA} />
+            <div className="px-2.5 sm:px-10 md:px-16 xl:px-[125px]">
+                <PostCodeSection serviceId={51}
+                    serviceName="Driveway Installation" />
+            </div>
+            {/* <DiscoverServices /> */}
             <Footer />
         </main>
     );
---
+++ src/app/component/BlogArchive/BlogDiscoverData.js
@@ -0,0 +1,60 @@
+export const BLOG_DATA = [
+    {
+        id: 1,
+        title: "Home & Garden",
+        image: "/homepage/image6.webp",
+        isSpecial: true,
+        bgColor: "bg-cyan-400",
+        slug: "/en/gb/home",
+    },
+    {
+        id: 2,
+        title: "Fence & Gate Installation",
+        image: "/homepage/fenceandgate.webp",
+        isSpecial: false,
+        slug: "/en/gb/fencing-contractors-near-me",
+
+    },
+    {
+        id: 3,
+        title: "Landscaping",
+        image: "/homepage/landscapingservice.webp", // Aapki image path
+        isSpecial: false,
+        slug: "/en/gb/landscape-gardeners-near-me",
+    },
+    {
+        id: 4,
+        title: "Artificial Grass Installation",
+        image: "/homepage/artificialgrassinstallation.webp",
+        isSpecial: false,
+        slug: "/en/gb/artificial-grass-installers-near-me",
+    },
+    {
+        id: 5,
+        title: "Patio Layer",
+        image: "/homepage/patiolayer.webp", // Aapki image path
+        isSpecial: false,
+        slug: "/en/gb/patio-layers-near-me",
+    },
+    {
+        id: 6,
+        title: "Gutter Cleaning",
+        image: "/homepage/guttercleaning.webp", // Aapki image path
+        isSpecial: false,
+        slug: "/en/gb/gutter-cleaning-near-me",
+    },
+    {
+        id: 7,
+        title: "Tree Surgery",
+        image: "/homepage/treesurgery.webp",
+        isSpecial: false,
+        slug: "/en/gb/tree-surgeon-near-me",
+    },
+    {
+        id: 8,
+        title: "Roofing",
+        image: "/roofing.webp",
+        isSpecial: false,
+        slug: "/en/gb/roofers-near-me",
+    }
+];
