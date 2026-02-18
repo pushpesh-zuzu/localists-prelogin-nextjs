f4098531658a1ec7fea33b8744b5f12573c10f6d
Deepak Birla <deepakbirla@zuzucodes.com>
2026-02-18 15:31:06 +0530

    update-status-code

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,11 +23,11 @@ export async function generateMetadata() {
       icon: "/favicon.ico",
     },
 
-    alternates: {
-      languages: {
-        "en-GB": "/en/gb",
-      },
-    },
+    // alternates: {
+    //   languages: {
+    //     "en-GB": "/en/gb",
+    //   },
+    // },
 
     openGraph: {
       locale: "en_GB",
