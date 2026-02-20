4cc5fd0f8cc5846874dfb1dd7585b6934d3514ea
Deepak Birla <deepakbirla@zuzucodes.com>
2026-02-20 18:32:03 +0530

    remove-pointer-cursor

--- src/app/component/Nearme/VettedSection.jsx
+++ src/app/component/Nearme/VettedSection.jsx
@@ -40,7 +40,7 @@ function VettedSection({
             ))}
             <div className="flex flex-wrap justify-center md:justify-start gap-[7px] gap-2.5 lg:gap-6 mb-2.5 mt-[30px] md:mt-[30px] lg:mt-12">
               <GetQuote variant="primary" text={getQuoteText} />
-              {extraButton && <Button variant="primary" className="py-[7px] xl:py-4 xl:px-[30px] cursor-pointer max-w-fit px-[13px]  hover:bg-[#00afe3]  rounded-full border-1 flex flex-row justify-center items-center gap-1 md:gap-[13px]"><UserIcon className="h-[18px] w-[18px] md:h-6 md:w-6" />View Roofers</Button>}
+              {extraButton && <Button variant="primary" className="py-[7px] xl:py-4 xl:px-[30px] max-w-fit px-[13px]  hover:bg-[#00afe3]  rounded-full border-1 flex flex-row justify-center items-center gap-1 md:gap-[13px]"><UserIcon className="h-[18px] w-[18px] md:h-6 md:w-6" />View Roofers</Button>}
             </div>
           </div>
         </div>
