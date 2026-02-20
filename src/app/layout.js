a20b33db8157102affa449207efae6b1690c70eb
Bade Harish <badeharish@zuzucodes.com>
2026-02-20 13:30:06 +0530

    update code on layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -57,7 +57,7 @@ export default function RootLayout({ children }) {
         suppressHydrationWarning
       >
         <StoreProvider>
-          {/* <Header /> */}
+          {/* <Header /> */} 
           <NoscriptTags />
           {children}
           {/* <TrackingScripts /> */}
