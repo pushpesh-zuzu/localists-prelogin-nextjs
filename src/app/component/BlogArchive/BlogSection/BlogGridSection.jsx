a965e9ad8a1786ada85c393a5f767a68fb8cc2c2
Bade Harish <badeharish@zuzucodes.com>
2026-04-29 14:01:51 +0530

    update blog internal links

--- src/app/component/BlogArchive/BlogSection/BlogGridSection.jsx
+++ src/app/component/BlogArchive/BlogSection/BlogGridSection.jsx
@@ -8,47 +8,49 @@ import GetQuote from "../../common/GetQuotes/GetQuote";
 
 
 const blogs = [
-    {
-        title: "Tree Removal Cost UK",
-        image: "/images/BlogPage/garden_shrubs.webp",
-    },
-    {
-        title: "New Roof Grants in 2026",
-        image: "/images/BlogPage/shrub1.webp",
-    },
+    // {
+    //     title: "Tree Removal Cost UK",
+    //     image: "/images/BlogPage/garden_shrubs.webp",
+    // },
+    // {
+    //     title: "New Roof Grants in 2026",
+    //     image: "/images/BlogPage/shrub1.webp",
+    // },
     {
         title: "How long does a flat roof last",
         image: "/images/BlogPage/Howlong.webp",
-        path:"/en/gb/blog/advice/how-long-does-a-flat-roof-last"
+        path: "/en/gb/blog/advice/how-long-does-a-flat-roof-last"
     },
     {
         title: "Roof Replacement Cost in 2026: The full Breakdown",
         image: "/images/BlogPage/view.webp",
+        path: "/en/gb/blog/cost-guides/roof-replacement-cost"
     },
     {
         title: "Roof repair cost in 2026: The full Breakdown",
         image: "/images/BlogPage/capMan.webp",
+        path: "/en/gb/blog/cost-guides/roof-repair-cost"
     },
-    {
-        title: "Roof cleaning cost in 2026: The full Breakdown",
-        image: "/images/BlogPage/waterMan.webp",
-    },
-    {
-        title: "Roof tile Replacement Costs in 2026: The full Breakdown",
-        image: "/images/BlogPage/DrillMan.webp",
-    },
-    {
-        title: "Roof inspection cost in 2026: The full Breakdown",
-        image: "/images/BlogPage/planPeoples.webp",
-    },
-    {
-        title: "Rubber roofing cost in 2026: The full Breakdown",
-        image: "/images/BlogPage/plates.webp",
-    },
-    {
-        title: "State vs tile roof: Which one is best for your home?",
-        image: "/images/BlogPage/frame.webp",
-    },
+    // {
+    //     title: "Roof cleaning cost in 2026: The full Breakdown",
+    //     image: "/images/BlogPage/waterMan.webp",
+    // },
+    // {
+    //     title: "Roof tile Replacement Costs in 2026: The full Breakdown",
+    //     image: "/images/BlogPage/DrillMan.webp",
+    // },
+    // {
+    //     title: "Roof inspection cost in 2026: The full Breakdown",
+    //     image: "/images/BlogPage/planPeoples.webp",
+    // },
+    // {
+    //     title: "Rubber roofing cost in 2026: The full Breakdown",
+    //     image: "/images/BlogPage/plates.webp",
+    // },
+    // {
+    //     title: "State vs tile roof: Which one is best for your home?",
+    //     image: "/images/BlogPage/frame.webp",
+    // },
 ];
 
 export default function BlogGridSection() {
@@ -57,10 +59,10 @@ export default function BlogGridSection() {
             <section className="max-w-[1320px] mx-auto px-4 md:px-8 xl:px-0 py-14">
                 <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
 
-                    <BlogHeroCard
+                    {/* <BlogHeroCard
                         title="ECO4 Axe Will Trigger a Wave of Business Collapse and Job Carnage, Expert Warns"
                         image="/images/BlogPage/Eco4.webp"
-                    />
+                    /> */}
 
                     {blogs.map((item, index) => (
                         <BlogCards
