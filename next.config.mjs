ddb17187a31c30ac97431739e676820a97a471bb
Bade Harish <badeharish@zuzucodes.com>
2026-05-05 23:45:55 +0530

    updated config file

--- next.config.mjs
+++ next.config.mjs
@@ -26,8 +26,8 @@ const nextConfig = {
 
   compress: true,
   poweredByHeader: false,
-  generateEtags: true, // improved caching validation
-  swcMinify: true, // faster minification
+  generateEtags: true, // improved caching validation ---> previous have false i changed to true for performance
+  swcMinify: true, // faster minification ---> newly added
 
   experimental: {
     optimizeCss: true,
@@ -48,14 +48,13 @@ const nextConfig = {
     //     pathname: "/**",
     //   },
     // ],
-    // formats: ["image/avif", "image/webp"],
     formats: ["image/avif", "image/webp"],
     deviceSizes: [320, 420, 640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
 
     loader: "default",
     qualities: [50, 75, 85, 90],
-    minimumCacheTTL: 86400,
+    minimumCacheTTL: 60,
   },
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
@@ -71,118 +70,9 @@ const nextConfig = {
           { key: "X-XSS-Protection", value: "1; mode=block" },
         ],
       },
-      {
-        source: "/_next/static/(.*)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-
-      {
-        source: "/images/(.*)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-
     ];
   },
 };
 
 export default nextConfig;
-
-
-
-
-
-
-
-
-
-
-
-
-// // next.config.mjs
-// import protectedRoutes from "./src/utils/protectedRoutes.mjs";
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   async redirects() {
-//     const ENV_COOKIE_DOMAIN =
-//       typeof process !== "undefined" &&
-//       process.env &&
-//       process.env.NEXT_PUBLIC_POST_LOGIN_URL
-//         ? process.env.NEXT_PUBLIC_POST_LOGIN_URL
-//         : null;
-//     // Map all protected routes to localhost:3001
-//     const redirects = protectedRoutes.map((route) => {
-//       // Remove leading slashes from route for consistency
-//       const cleanRoute = route.replace(/^\/+/, "");
-//       return {
-//         source: `/${cleanRoute}/:path*`, // source must start with /
-//         destination: `${ENV_COOKIE_DOMAIN}${cleanRoute}/:path*`, // destination with trailing slash
-//         permanent: false,
-//       };
-//     });
-
-//     return redirects;
-//   },
-
-//   compress: true,
-//   poweredByHeader: false,
-//   generateEtags: false,
-
-//   experimental: {
-//     optimizeCss: true,
-//     optimizePackageImports: ["lucide-react", "date-fns"],
-//   },
-
-//   images: {
-//     // domains: ["dev.localists.com"],
-//     domains: [
-//       process.env.NEXT_PUBLIC_APP_BASE_URL
-//         ? new URL(process.env.NEXT_PUBLIC_APP_BASE_URL).hostname
-//         : "dev.localists.com",
-//     ],
-//     // remotePatterns:[
-//     //    {
-//     //     protocol: "https",
-//     //     hostname: "dev.localists.com",
-//     //     pathname: "/**",
-//     //   },
-//     // ],
-//     formats: ["image/avif", "image/webp"],
-//     formats: ["image/avif", "image/webp"],
-//     deviceSizes: [320, 420, 640, 750, 828, 1080, 1200],
-//     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-
-//     loader: "default",
-//     qualities: [50, 75, 85, 90],
-//     minimumCacheTTL: 60,
-//   },
-//   compiler: {
-//     removeConsole: process.env.NODE_ENV === "production",
-//   },
-
-//   headers: async () => {
-//     return [
-//       {
-//         source: "/(.*)",
-//         headers: [
-//           { key: "X-Content-Type-Options", value: "nosniff" },
-//           { key: "X-Frame-Options", value: "DENY" },
-//           { key: "X-XSS-Protection", value: "1; mode=block" },
-//         ],
-//       },
-//     ];
-//   },
-// };
-
-// export default nextConfig;
 
