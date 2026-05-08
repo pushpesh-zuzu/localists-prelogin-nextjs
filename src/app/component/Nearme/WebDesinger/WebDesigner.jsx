4c00e1acc0908678efbe483866afe0c25157f94a
Bade Harish <badeharish@zuzucodes.com>
2026-05-08 19:29:08 +0530

    add bookkeeeping member image

--- src/app/component/Nearme/WebDesinger/WebDesigner.jsx
+++ src/app/component/Nearme/WebDesinger/WebDesigner.jsx
@@ -99,6 +99,11 @@ function WebDesigner() {
             desktopImage="/nearme/WebsiteDesingner/webDesingMebmerDesktop.webp"
             mobileImage="/nearme/WebsiteDesingner/webDesingMebmerMobile.webp"
                 description={`“The thing I like about Localists is they don’t tie you down to any monthly contracts and the leads are generally better.  When I’m quiet, I just hop on the website and get a few leads to tie me over.”`}
+<<<<<<< HEAD:src/app/component/Nearme/WebDesinger/WebDesigner.jsx
+=======
+                desktopImage="/nearme/BookKeeping/bookkeeping-member.webp"
+                mobileImage="/nearme/BookKeeping/bookkeeping-member.webp"
+>>>>>>> 92888b6c (add bookkeeeping member image):src/app/component/Nearme/BookKeepersNearMe/BookKeepersNearMe.jsx
             />
             <Footer />
         </>
